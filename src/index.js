8f5b9758015ecccd3766e4024a51ceed01bcacea
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 15:23:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
-import ReactDOM from  "react-dom";
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom";
+import App from './App';
+import './index.css';
 
-=======
-import React from "react";
-import ReactDOM from  "react-dom";
-import App from './App'
-import './index.css'
-
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
 ReactDOM.render(<App/>, document.getElementById('root'));
