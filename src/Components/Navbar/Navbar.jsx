0ebb108a8f6b6a0f6ced91e57f062b984a6170cd
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-12 00:35:05 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../logo.svg';
-import './navbar.css';
+import './Navbar.css';
 
 const Navbar = () => {
   // const [toggleMenu, setToggleMenu] = useState(false);
