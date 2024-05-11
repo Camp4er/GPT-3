e64c564d050fc971a0f8785256e3d2078507c737
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 15:40:13 +0530

    Update CTA.jsx

--- src/Components/CTA/CTA.jsx
+++ src/Components/CTA/CTA.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './CTA.css';
 
@@ -14,21 +13,4 @@ const CTA = () => (
   </div>
 );
 
-=======
-import React from 'react';
-import './CTA.css';
-
-const CTA = () => (
-  <div className="gpt3__cta">
-    <div className="gpt3__cta-content">
-      <p>Request Early Access to Get Started</p>
-      <h3>Register Today & start exploring the endless possibilities.</h3>
-    </div>
-    <div className="gpt3__cta-btn">
-      <button type="button">Get Started</button>
-    </div>
-  </div>
-);
-
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
 export default CTA;
