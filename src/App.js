94185cd32b89398d2a939c780978596b7649ea78
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 15:16:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
-<<<<<<< HEAD
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-import { Brand, CTA, Navbar } from './Components'
-import { Footer, Blog, Possibility, Features, whatGPT3, Header} from './Container'
-
+import { Brand, CTA, Navbar } from './Components';
+import { Footer, Blog, Possibility, Features, whatGPT3, Header } from './Container';
 
 const App = () => {
   return (
     <div className='App'>
       <div className='gradient_bg'>
-        
-        <Navbar/>
-        
-        <Header/>
+        <Navbar />
+        <Header />
       </div>
-      <Brand/>
-      <whatGPT3/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
+      <Brand />
+      <whatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
-=======
-import React from 'react'
-import './App.css'
-
-import { Brand, CTA, Navbar } from './Components'
-import { Footer, Blog, Possibility, Features, whatGPT3, Header} from './Container'
-
-
-const App = () => {
-  return (
-    <div className='App'>
-      <div className='gradient_bg'>
-        
-        <Navbar/>
-        
-        <Header/>
-      </div>
-      <Brand/>
-      <whatGPT3/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
-    </div>
-  )
-}
-
-export default App
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
+export default App;
