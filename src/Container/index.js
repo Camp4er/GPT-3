2eab8ab2df429111a84125e9f136237cd817f79c
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 18:31:02 +0530

    Update index.js

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export { default as Blog } from './Blog/Blog'
 export { default as Features } from './Features/Features'
 export { default as Footer } from './Footer/Footer'
 export { default as Header } from './Header/Header'
 export { default as Possibility } from './Possibility/Possibility'
 export { default as whatGPT3 } from './whatGPT3/whatGPT3'
-=======
-export { default as Blog } from './Blog/Blog'
-export { default as Features } from './Features/Features'
-export { default as Footer } from './Footer/Footer'
-export { default as Header } from './Header/Header'
-export { default as Possibility } from './Possibility/Possibility'
-export { default as whatGPT3 } from './whatGPT3/whatGPT3'
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
