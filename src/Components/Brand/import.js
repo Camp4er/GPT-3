f156f5198bcc084b061f0be00d7c0e1b86ccf7ca
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 15:38:22 +0530

    Update import.js

--- src/Components/Brand/import.js
+++ src/Components/Brand/import.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import google from '../../assets/google.png';
 import slack from '../../assets/slack.png';
 import atlassian from '../../assets/atlassian.png';
@@ -11,18 +10,4 @@ export {
   atlassian,
   dropbox,
   shopify,
-=======
-import google from '../../assets/google.png';
-import slack from '../../assets/slack.png';
-import atlassian from '../../assets/atlassian.png';
-import dropbox from '../../assets/dropbox.png';
-import shopify from '../../assets/shopify.png';
-
-export {
-  google,
-  slack,
-  atlassian,
-  dropbox,
-  shopify,
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
 };
