a685bcdfcde0188512597ee18c70543e1a878f16
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 18:33:16 +0530

    Update whatGPT3.jsx

--- src/Container/whatGPT3/whatGPT3.jsx
+++ src/Container/whatGPT3/whatGPT3.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Feature from '../../Components/Feature/Feature';
 import './whatGPT3.css';
@@ -20,27 +19,4 @@ const whatGPT3 = () => (
   </div>
 );
 
-=======
-import React from 'react';
-import Feature from '../../Components/Feature/Feature';
-import './whatGPT3.css';
-
-const whatGPT3 = () => (
-  <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
-    <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
-    </div>
-    <div className="gpt3__whatgpt3-heading">
-      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
-      <p>Explore the Library</p>
-    </div>
-    <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-    </div>
-  </div>
-);
-
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
 export default whatGPT3;
