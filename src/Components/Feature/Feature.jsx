30559bdf719b93cb4ed49ac1f4cba6576aea15b3
Camp4er <143322030+Camp4er@users.noreply.github.com>
2024-05-11 15:45:23 +0530

    Update Feature.jsx

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './Feature.css';
 
@@ -14,21 +13,4 @@ const Feature = ({ title, text }) => (
   </div>
 );
 
-=======
-import React from 'react';
-import './Feature.css';
-
-const Feature = ({ title, text }) => (
-  <div className="gpt3__features-container__feature">
-    <div className="gpt3__features-container__feature-title">
-      <div />
-      <h1>{title}</h1>
-    </div>
-    <div className="gpt3__features-container_feature-text">
-      <p>{text}</p>
-    </div>
-  </div>
-);
-
->>>>>>> abb6cdcb667df92cf953df7054999e17c717296b
 export default Feature;
